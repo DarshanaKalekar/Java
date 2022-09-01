a7459db4df57c13364fc4393392222546ac54256
DarshanaKalekar <77119649+DarshanaKalekar@users.noreply.github.com>
2022-09-01 10:20:11 +0530

    Create TightCoupling.java

---
+++ TightCoupling.java
@@ -0,0 +1,18 @@
+public class TightCoupling {
+    class Volume
+    {
+        public static void main(String args[])
+        {
+            Box b = new Box(5,5,5);
+            System.out.println(b.volume);
+        }
+    }
+    class Box
+    {
+        public int volume;
+        Box(int length, int width, int height)
+        {
+            this.volume = length * width * height;
+        }
+    }
+}
